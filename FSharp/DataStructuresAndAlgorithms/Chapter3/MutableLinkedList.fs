dc0b98456f21e101bdea21cabe4e4c4c13fd4817
asmichi <asmichi@users.noreply.github.com>
2017-02-26 19:24:03 +0900

    Teach myself F# with "Data Structures and Algotithms".

---
+++ FSharp/DataStructuresAndAlgorithms/Chapter3/MutableLinkedList.fs
@@ -0,0 +1,194 @@
+﻿module DataStructuresAndAlgorithms.Chapter3.MutableLinkedList
+open Xunit
+open Swensen.Unquote
+open System.Collections
+open System.Collections.Generic
+
+// Properties: https://docs.microsoft.com/en-us/dotnet/articles/fsharp/language-reference/members/properties
+
+//type MutableLinkedListNode0<'a> = 
+//    val mutable private value : 'a
+//    val mutable private next : MutableLinkedListNode0<'a> option
+
+//    new (value, next) =
+//        {value = value; next = next}
+//    member this.Value 
+//        with get() = this.value
+//        and  set v = this.value <- v
+
+//    member this.Next 
+//        with get() = this.next
+//        and  set v = this.next <- v
+
+type MutableLinkedListNode<'a>(value : 'a, next : MutableLinkedListNode<'a> option) =
+    member val public Value = value with get, set
+    member val public Next = next with get, set
+
+    member public this.LastNode =
+        match this.Next with
+        | Some n -> n.LastNode
+        | None -> this
+
+    member public this.InsertAfterThis v =
+        let newNext = MutableLinkedListNode<'a>(v, this.Next)
+        this.Next <- Some(newNext)
+        this
+
+    member public this.RemoveLast =
+        match this.Next with
+        | Some n ->
+            match n.Next with
+            | Some nn -> nn.RemoveLast
+            | None -> this.Next <- None
+        | None -> failwith "No successor."
+
+    member public this.RemoveNode n =
+        match this.Next with
+        | Some nextNode ->
+            if nextNode = n then
+                this.Next <- nextNode.Next
+            else
+                nextNode.RemoveNode n
+        | None -> failwith "element not found"
+
+    member public this.Nth i =
+        if i > 0 then
+            match this.Next with
+            | Some n -> n.Nth (i - 1)
+            | None -> failwith "element not found"
+        else
+            this
+
+type MutableLinkedList<'a> =
+    interface IEnumerable<'a> with
+        member this.GetEnumerator(): IEnumerator<'a> = 
+            this.ToSeq.GetEnumerator()
+        member this.GetEnumerator(): IEnumerator = 
+            (this.ToSeq :> IEnumerable).GetEnumerator()
+        
+    val mutable private head : MutableLinkedListNode<'a> option
+    new () = MutableLinkedList(None)
+    new (head) = { head = head }
+
+    member public this.ToSeq = 
+        this.head
+        |> Seq.unfold (fun n -> 
+            match n with
+            | Some n -> Some(n.Value, n.Next)
+            | None -> None)
+
+    member private this.FirstNode =
+        this.head
+
+    member private this.LastNode = 
+        match this.head with
+        | Some n -> Some(n.LastNode)
+        | None -> None
+
+    member public this.Tail =
+        match this.head with
+        | Some x -> Some(MutableLinkedList(x.Next))
+        | None -> None
+
+    member public this.AddFirst v =
+        this.head <- Some(MutableLinkedListNode(v, this.head))
+        this
+
+    member public this.AddLast v =
+        let next = Some(MutableLinkedListNode(v, None))
+        match this.head with
+            | Some n -> n.LastNode.Next <- next
+            | None -> this.head <- next
+        this
+
+    member public this.RemoveFirst =
+        this.head <- 
+            match this.head with
+            | Some n -> n.Next
+            | None -> failwith "List is empty."
+        this
+
+    member public this.RemoveLast =
+        match this.head with
+        | Some n ->
+            match n.Next with
+            | Some nn -> n.RemoveLast |> ignore
+            | None -> this.head <- None
+        | None -> failwith "List is empty."
+        this
+
+    member public this.RemoveNode n =
+        match this.head with
+        | Some h ->
+            if h = n then
+                this.head <- h.Next
+            else
+                h.RemoveNode n
+        | None -> failwith "List is empty."
+        this
+
+    member public this.NthNode i =
+        match this.head with
+        | Some n -> n.Nth i
+        | None -> failwith "List is empty."
+
+    member public this.AddAfter (n : MutableLinkedListNode<'a>) v =
+        n.InsertAfterThis v |> ignore
+        this
+
+module MutableLinkedList =
+    let toSeq (xs : MutableLinkedList<'a>) = xs.ToSeq
+    let fromSeq<'a> : ('a seq -> MutableLinkedList<'a>) =
+        MutableLinkedList<'a>()
+        |> Seq.fold (fun list x -> list.AddLast(x))
+
+[<Fact>]
+let ``Can construct from seq``() = 
+    let sut = MutableLinkedList.fromSeq [ 1; 2 ]
+
+    (Seq.toList sut) =! [ 1; 2 ]
+
+[<Fact>]
+let ``Can add element at the front``() = 
+    let sut = MutableLinkedList.fromSeq [ 1 ]
+
+    sut.AddFirst(2) |> ignore
+
+    (Seq.toList sut) =! [ 2; 1 ]
+
+
+[<Fact>]
+let ``Can add element at the end``() = 
+    let sut = MutableLinkedList.fromSeq [ 1 ]
+
+    sut.AddLast(2) |> ignore
+
+    (Seq.toList sut) =! [ 1; 2 ]
+
+[<Fact>]
+let ``Can remove element at the front``() = 
+    let sut = MutableLinkedList.fromSeq [ 1; 2 ]
+
+    sut.RemoveFirst |> ignore
+    (Seq.toList sut) =! [ 2 ]
+
+    sut.RemoveFirst |> ignore
+    (Seq.toList sut) =! [ ]
+
+[<Fact>]
+let ``Can remove element at the end``() = 
+    let sut = MutableLinkedList.fromSeq [ 1; 2 ]
+
+    sut.RemoveLast |> ignore
+    (Seq.toList sut) =! [ 1 ]
+
+    sut.RemoveLast |> ignore
+    (Seq.toList sut) =! [ ]
+
+[<Fact>]
+let ``Can remove element in the middle``() = 
+    let sut = MutableLinkedList.fromSeq [ 1; 2; 3 ]
+
+    sut.RemoveNode (sut.NthNode 1) |> ignore
+    (Seq.toList sut) =! [ 1; 3 ]
+
---
+++ FSharp/DataStructuresAndAlgorithms/Scripts/load-references-debug.fsx
@@ -0,0 +1,10 @@
+// Warning: generated file; your changes could be lost when a new file is generated.
+#I __SOURCE_DIRECTORY__
+#r "System.Core.dll"
+#r "System.dll"
+#r "System.Numerics.dll"
+#r "../packages/Unquote.3.1.2/lib/net45/Unquote.dll"
+#r "../packages/xunit.runner.visualstudio.2.1.0/build/_common/xunit.abstractions.dll"
+#r "../packages/xunit.assert.2.1.0/lib/dotnet/xunit.assert.dll"
+#r "../packages/xunit.extensibility.core.2.1.0/lib/dotnet/xunit.core.dll"
+#r "../packages/xunit.extensibility.execution.2.1.0/lib/net45/xunit.execution.desktop.dll"
