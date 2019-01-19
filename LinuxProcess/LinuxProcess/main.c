04d7bc6b6c1e02fe65477c83d2f4772b070f97a2
asmichi <asmichi@users.noreply.github.com>
2019-01-19 20:34:18 +0900

    Fix

--- LinuxProcess/LinuxProcess/main.c
+++ LinuxProcess/LinuxProcess/main.c
@@ -2,23 +2,35 @@
 #include <stdio.h>
 #include <string.h>
 
+int appagent(int argc, const char** argv);
 int app1(int argc, const char** argv);
 int app2(int argc, const char** argv);
 int appmain(int argc, const char** argv);
 
+struct SubCommandDefinition
+{
+    const char* Name;
+    int(*Func)(int, const char**);
+};
+
+static struct SubCommandDefinition SubCommandDefinitions[] =
+{
+    {"app1", app1},
+    {"app2", app1},
+};
+
+
 int main(int argc, const char** argv)
 {
-    if (strcmp(argv[0], "app1") == 0)
+    for (int i = 0; i < sizeof(SubCommandDefinitions) / (sizeof(SubCommandDefinitions[0])); i++)
     {
-        return app1(argc, argv);
-    }
-    else if (strcmp(argv[0], "app2") == 0)
-    {
-        return app2(argc, argv);
-    }
-    else
-    {
-        return appmain(argc, argv);
-    }
-}
+        const struct SubCommandDefinition* d = &SubCommandDefinitions[i];
 
+        if (strcmp(argv[0], d->Name) == 0)
+        {
+            return (d->Func)(argc, argv);
+        }
+    }
+
+    return appmain(argc, argv);
+}
